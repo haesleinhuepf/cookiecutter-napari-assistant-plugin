5f22706ba5fc626acfb7af869fc083f5812d91ae
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-10-29 17:00:44 +0200

    expose major function in public  API

--- {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.plugin_name}}/{{cookiecutter.module_name}}/__init__.py
@@ -1,5 +1,7 @@
 __version__ = "0.1.0"
 
+from ._function import process_image
+
 {% if cookiecutter.include_reader_plugin == 'y' %}from ._reader import napari_get_reader{% endif %}
 {% if cookiecutter.include_writer_plugin == 'y' %}from ._writer import napari_get_writer, napari_write_image{% endif %}
 {% if cookiecutter.include_dock_widget_plugin == 'y' %}from ._dock_widget import napari_experimental_provide_dock_widget{% endif %}
